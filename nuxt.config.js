e1a2a97fd363669685f53bc5cb32b6fd9d11a25f
Gilgamesh-y <805655528@qq.com>
2021-02-25 22:38:21 +0800

    feat

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [
+        [ "component", {"libraryName": "element-ui",  "styleLibraryName": "theme-chalk"}] 
+      ]
+    },
   }
 }
